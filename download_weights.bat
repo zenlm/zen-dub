4dceb0c044c698446384780f89ee5f5d1c9e214c
Zach Kelling <z@zeekay.io>
2026-06-16 21:29:40 +0000

    zen-dub: native lip-sync video dub (Apache-2.0)

---
+++ download_weights.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal
+
+:: Set the checkpoints directory
+set CheckpointsDir=models
+
+:: Create necessary directories
+mkdir %CheckpointsDir%\zen-dub
+mkdir %CheckpointsDir%\zen-dubV15
+mkdir %CheckpointsDir%\syncnet
+mkdir %CheckpointsDir%\dwpose
+mkdir %CheckpointsDir%\face-alignment
+mkdir %CheckpointsDir%\sd-vae-ft-mse
+mkdir %CheckpointsDir%\whisper
+
+:: Install required packages
+pip install -U "huggingface_hub[hf_xet]"
+
+:: Set HuggingFace endpoint
+set HF_ENDPOINT=https://hf-mirror.com
+
+:: Download Zen-Dub weights
+hf download hanzoai/Zen-Dub --local-dir %CheckpointsDir%
+
+:: Download SD VAE weights
+hf download stabilityai/sd-vae-ft-mse --local-dir %CheckpointsDir%\sd-vae --include "config.json" "diffusion_pytorch_model.bin"
+
+:: Download Whisper weights
+hf download openai/whisper-tiny --local-dir %CheckpointsDir%\whisper --include "config.json" "pytorch_model.bin" "preprocessor_config.json"
+
+:: Download DWPose weights
+hf download yzd-v/DWPose --local-dir %CheckpointsDir%\dwpose --include "dw-ll_ucoco_384.pth"
+
+:: Download SyncNet weights
+hf download ByteDance/LatentSync --local-dir %CheckpointsDir%\syncnet --include "latentsync_syncnet.pt"
+
+:: Download 2D-FAN-4 facial-landmark weights (used to build the face blend mask)
+curl -L https://www.adrianbulat.com/downloads/python-fan/2DFAN4-cd938726ad.zip -o %CheckpointsDir%\face-alignment\2DFAN4-cd938726ad.pth
+
+echo All weights have been downloaded successfully!
+endlocal
